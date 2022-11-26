9713a714a1f3ef48af51aeeeb4039ce3dd0565f1
Anzori Ghurtchumelia <anzori.gurchumelia@gau.edu.ge>
2022-11-26 21:27:05 +0400

    add syntax for functors

--- src/main/scala/part1recap/TypeClassRecap.scala
+++ src/main/scala/part1recap/TypeClassRecap.scala
@@ -28,14 +28,37 @@ object TypeClassRecap {
 
       given optionFunctor: MyFunctor[Option] with
         override def map[A, B](fa: Option[A])(f: A => B): Option[B] = fa.map(f)
+
+      object MyFunctorSyntax {
+        extension[F[_]: MyFunctor, A](self: F[A])
+          def map[B](f: A => B): F[B] = MyFunctor[F].map(self)(f)
+      }
     }
 
     import MyFunctor.*
+    import MyFunctor.MyFunctorSyntax.*
 
-    def map[F[_]: MyFunctor, A, B](fa: F[A])(f: A => B): F[B] = MyFunctor[F].map(fa)(f)
+    def map[F[_]: MyFunctor, A, B](fa: F[A])(f: A => B): F[B] = fa.map(f)
 
     val result1 = map[Option, Int, String](Option(5))(_.toString)
     val result2 = map[List, String, Boolean]("123" :: "345" :: "333" :: Nil)(_.contains("3"))
+
+  }
+
+  object cats_functor {
+
+    import cats.Functor
+    import cats.instances.option._
+    import cats.instances.list._
+    import cats.instances.int._
+    import cats.instances.string._
+
+    import cats.syntax.functor._
+
+    def map[F[_]: Functor, A, B](fa: F[A])(f: A => B): F[B] = fa map f
+
+    map[Option, Int, String](Option(5))(_.toString)
+    map[List, Int, Int](1 :: 2 :: 3 :: Nil)(_ + 1)
 
   }
 
