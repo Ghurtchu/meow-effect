a7f1b803d6767578cf5da96aafc4a27ade2d8059
Anzori Ghurtchumelia <anzori.gurchumelia@gau.edu.ge>
2022-11-28 16:12:08 +0400

    progress

--- src/main/scala/part2effects/CatsIOIntro.scala
+++ src/main/scala/part2effects/CatsIOIntro.scala
@@ -2,6 +2,18 @@ package part2effects
 
 object CatsIOIntro {
 
+  import cats.effect.IO
 
+  // IO
+  val firstIO: IO[Int] = IO.pure[Int](42) // lifts Int into IO[Int] this is not lazy, it's eagerly evaluated like ojb.apply[A](a: A): otherobj[A] = otherobj(a)
+
+  // side-effecting delayed computation
+  val delayedIO: IO[Int] = IO.delay[Int] {
+    Thread sleep 125
+
+    42
+  }
+
+  val delayedIO2: IO[String] = IO("Hey there") // sugar syntax for IO.apply
 
 }
