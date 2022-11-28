509b64cf4312255530bd1f4c104fd01d0ffcd00e
Anzori Ghurtchumelia <anzori.gurchumelia@gau.edu.ge>
2022-11-28 15:39:12 +0400

    implement simple IO monad

--- src/main/scala/part2effects/Effects.scala
+++ src/main/scala/part2effects/Effects.scala
@@ -1,5 +1,8 @@
 package part2effects
 
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
+
 object Effects {
 
   // pure functional programming
@@ -19,6 +22,38 @@ object Effects {
   var anInt = 0
   val value: Unit = (anInt += 1) // side effect
   val value_2: Unit = () // breaks RT
+
+  // side effects are inevitable
+
+  // effects (cats IO, ZIO etc)
+
+  /**
+   * Desires:
+   * - type signature describes what kind of computation will be performed if we evaluate it
+   * - what kind of value will be produced by applying that calculation
+   * - when side effects are needed, effect construction is separate of effect execution
+   */
+
+  // Option
+  // 1 - checks (describes potentially absent value)
+  // 2 - checks (we know that it's either Some(Int) or None)
+  // 3 - checks (side effects are not needed)
+  val anOption: Option[Int] = Option(42)
+
+  // Future
+  // 1 - checks (describes async computation)
+  // 2 - checks (either Success[Int] or Failure[Throwable])
+  // 3 - does not check (allocating or scheduling thread pool is required to run Future) Execution is not separate from construction
+  import scala.concurrent.ExecutionContext.Implicits.global
+  val aFuture: Future[Int] = Future(42)
+
+  // our IO data type
+  // and it's a fair-like-square effect :)
+  final case class IOMonad[A](unsafeRun: () => A) {
+    def map[B](f: A => B): IOMonad[B] = new IOMonad[B](() => f(unsafeRun()))
+    def flatMap[B](f: A => IOMonad[B]): IOMonad[B] = new IOMonad[B](() => f(unsafeRun()).unsafeRun())
+  }
+
 
   def main(args: Array[String]): Unit = {
 
